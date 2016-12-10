e89b6b71cdb9024b708a9eda61bf16761608cb5f
PseudoscienceGames <quade81@gmail.com>
2016-12-10 17:53:49 -0600

    Old Prototype Files

---
+++ Assets/Scripts/PropScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropScript : MonoBehaviour
+{
+    public int dir;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, ((transform.root.GetComponent<QuadScript>().throttle + 1) * 1000 * dir), 0);
+    }
+}
---
+++ Assets/Scripts/QuadScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class QuadScript : MonoBehaviour
+{
+    public float throttle;
+    public float yaw;
+    public float pitch;
+    public float roll;
+    public float thrust;
+
+    public float rotScale;
+
+    public Rigidbody myRB;
+    public GameObject myCam;
+
+    void Update()
+    {
+        throttle = Input.GetAxis("Throttle") + 1;
+        pitch = Mathf.Pow(Input.GetAxis("Pitch"), 3);
+        roll = Mathf.Pow(Input.GetAxis("Roll"), 3);
+        yaw = Mathf.Pow(Input.GetAxis("Yaw"), 3);
+
+        transform.Rotate(new Vector3(pitch, yaw, -roll) * rotScale);
+
+        if (Input.GetButtonDown("Fire1"))
+            PhotonNetwork.Instantiate("Bullet", transform.position, transform.rotation, 0);
+    }
+
+    void FixedUpdate()
+    {
+        thrust = Mathf.Pow(throttle, 2) * -Physics.gravity.y * myRB.mass;
+        myRB.AddForce(transform.up * thrust);
+    }
+}
